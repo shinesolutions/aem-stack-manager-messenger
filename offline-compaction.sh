e0401aa044cb1674abbc9c81ccba44309a554c03
Andy Wang <andy.wang@shinesolutions.com>
2017-06-07 15:14:32 +1000

    merging an externalId in message if provided

--- offline-compaction.sh
+++ offline-compaction.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xv
 
 
-make offline-snapshot stack_prefix=aem62test topic_config_file=topic_config.yaml message_config_file=inventory/group_vars/offline-snapshot.yaml
+make offline-compaction-snapshot stack_prefix=aem62test topic_config_file=topic_config.yaml message_config_file=inventory/group_vars/offline-compaction-snapshot.yaml
